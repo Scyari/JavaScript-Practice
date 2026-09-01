83046fde9be9e17f95dbdf5f70f3ec5b1cf684f2
Scyari <arindamsah415@gmail.com>
2026-09-02 01:04:12 +0530

    for-of-loop

--- All-Kind-Of-loops/10-loopchallenges.js
+++ All-Kind-Of-loops/10-loopchallenges.js
@@ -33,3 +33,42 @@ for (let i = 0; i < place.length; i++) {
     visitedCities.push(place[i]);
 }
 // console.log(visitedCities);        //output --> [ 'london', 'new york', 'berlin' ]
+
+/*
+
+3. Write a "for -of" loop to iterate through the array [1, 2, 3, 4, 5] and stop when the number "4" is found.
+
+   store the numbers before "4" in a new array named "smallNumbers".
+   */
+
+let numbers = [1, 2, 3, 4, 5];
+let smallNumber = [];
+
+for (let i = 0; i < numbers.length; i++) {
+
+    if (numbers[i] === 4) {
+        break;                    //stop the loop when      number 4 is found
+    }
+    smallNumber.push(numbers[i]);
+}
+// console.log(smallNumber);       //output --> [ 1, 2, 3 ]
+
+/*
+
+4. Use a "for -of" loop to iterate through the array ["apple", "banana", "cherry", "date"] and skip "cherry".
+
+   store the other fruits in a new array named "preferredFruits".
+*/
+
+let fruits = ["apple", "banana", "cherry", "date"];
+let preferredFruits = [];
+
+for (const fruit of fruits) {            // carrying fruit as const of fruits
+    if (fruit === "cherry") {           // calling const (fruit)
+        continue;
+    }
+    preferredFruits.push(fruit);      // calling const fruit
+}
+// console.log(preferredFruits);    //output [ 'apple', 'banana', 'cherry', 'date' ]
+
+
