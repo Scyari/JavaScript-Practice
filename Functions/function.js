b249c7859775516a4101bffbc42ed9b9c96794ad
Scyari <arindamsah415@gmail.com>
2026-09-10 01:19:38 +0530

    function and parameter

---
+++ Functions/function.js
@@ -0,0 +1,17 @@
+/* 
+1. Write a function named "makeTea" that takes one parameter.
+"typeOfTea", and returns a string like 
+"making Lemon tea", when called with "Lemon tea".
+store the result in a variable named "teaOrder".
+*/
+
+// function greet(name) {
+//     console.log(`hello $ {name}`);
+// }
+
+function makeTea(typeOfTea) {
+    return `Making ${typeOfTea}`;
+    console.log(test);  //unreachable code
+}
+let teaOrder = makeTea("Lemon Tea");  // argument
+console.log(teaOrder);    //output --> Making Lemon Tea
