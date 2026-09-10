6d8164f65bb23e21760d6b1d7073072da0733e9a
Scyari <arindamsah415@gmail.com>
2026-09-11 01:04:39 +0530

    function inside function

--- Functions/function.js
+++ Functions/function.js
@@ -9,9 +9,24 @@ store the result in a variable named "teaOrder".
 //     console.log(`hello $ {name}`);
 // }
 
-function makeTea(typeOfTea) {
+function makeTea(typeOfTea) {        // (typeOfTea) --> parameter
     return `Making ${typeOfTea}`;
     console.log(test);  //unreachable code
 }
-let teaOrder = makeTea("Lemon Tea");  // argument
-console.log(teaOrder);    //output --> Making Lemon Tea
+let teaOrder = makeTea("Lemon Tea");      // argument
+// console.log(teaOrder);                //output --> Making Lemon Tea
+
+/* 
+2. write a function named "ordertea" that takes one parameters "teaType". inside this function create another function named "confirmOrder" that return a message 
+like "order confirmed for chai".
+  call "confirmOrder" from within "orderTea" and return the result. 
+*/
+function orderTea(teaType) {
+    function confirmOrder() {                  // function inside function
+        return `order confirmed for chai`;     //return value
+    }
+    return confirmOrder()      // calling function within function
+}
+let orderConfirmation = orderTea("Black Coffee");   // argument
+console.log(orderConfirmation);         //output-- > order confirmed for chai
+
