03c7cf8fdb0122a91661b50f36b8edab8a3bb8b2
Scyari <arindamsah415@gmail.com>
2026-09-16 01:49:46 +0530

    function returns another function

--- Functions/function.js
+++ Functions/function.js
@@ -63,4 +63,17 @@ let order = processTeaOrder(makeTea)
 
 
 /*
-5.
+5.Write a function named "createTeaMaker" that returns another function. The returned function. The returned function should take one parameter, "teaType", and return a message like "Making green tea".
+     store the returned function in a variable named "teaMaker" and call it with "green tea".
+*/
+
+function createTeamaker(name) {
+    return function (teaType) {
+        return `Making: ${teaType}`;
+    };
+}
+
+let teaMaker = createTeamaker();
+// let result = teaMaker("green tea");
+// console.log(result);                         //output-- > Making: green tea
+console.log(teaMaker("green tea"));             //output-- > Making: green tea
