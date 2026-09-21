6caa0ca18eb8f2af76d9bd30c6a871cd9cfac3ae
Scyari <arindamsah415@gmail.com>
2026-09-22 01:52:48 +0530

    using dunder method

---
+++ OOPs/prototypes.js
@@ -0,0 +1,12 @@
+let computer = { cpu: 12 };
+let samsung = {
+    screen: "hd",
+    __proto__: computer       // __(this called dunder)
+
+};
+let lg = {};
+
+// console.log(`samsung`, samsung.__proto__); //Acces prototype of any particular object
+
+
+
